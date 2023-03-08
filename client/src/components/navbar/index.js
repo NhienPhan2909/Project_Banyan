e2c96deb6361fb70f1915e3b81a85eab92084f32
lat71 <lat71@case.edu>
2023-03-08 00:30:31 -0500

    JWT token now keeps you signed in. Changes to navbar to differentiate signed in vs signedout

--- client/src/components/navbar/index.js
+++ client/src/components/navbar/index.js
@@ -1,19 +1,58 @@
 import React from "react";
 import { Nav, NavLink, NavMenu }
 	from "./elements";
+import Buffer from "buffer";
+
+
+function checkJwtToken() {
+	// Get the JWT token from local storage
+	const token = localStorage.getItem('jwtToken');
+  
+	if (!token) {
+	  // Token not found in local storage
+	  return false;
+	}
+  
+	// Decode the JWT token
+	const base64Url = token.split('.')[1];
+	const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+	const decodedToken = JSON.parse(Buffer.Buffer.from(base64, 'base64').toString());
+	const expTime = decodedToken.exp * 1000; // convert to ms
+  
+	// Check if the token is expired, return true if not
+	return Date.now() < (expTime) 
+  }
+  
 
 const Navbar = () => {
 return (
 	<>
 	<Nav>
 		<NavMenu>
-		<NavLink to="/signup" activeStyle>
-			Signup
-		</NavLink>
-		<NavLink to="/login" activeStyle>
-			Login
-		</NavLink>
-		<NavLink to="/dashboard" activeStyle>
+		{checkJwtToken() ? 
+			(
+			<>
+				<NavLink to="/dashboard" activeStyle>
+					Dashboard
+				</NavLink>
+				<NavLink to="/prompt" activeStyle>
+					Prompt
+				</NavLink>
+				<NavLink to="/tree" activeStyle>
+					Tree
+				</NavLink> 
+			</>
+			): (
+			<>
+				<NavLink to="/signup" activeStyle>
+					Signup
+				</NavLink>
+				<NavLink to="/login" activeStyle>
+					Login
+				</NavLink>
+			</>
+			)}
+		{/* <NavLink to="/dashboard" activeStyle>
 			Dashboard
 		</NavLink>
 		<NavLink to="/prompt" activeStyle>
@@ -21,7 +60,7 @@ return (
 		</NavLink>
 		<NavLink to="/tree" activeStyle>
 			Tree
-		</NavLink>
+		</NavLink> */}
 		</NavMenu>
 	</Nav>
 	</>
