e1c74e077e15d271966931dd890d3e5c3fc8f9be
Nhien Phan <nhienphanhoang@gmail.com>
2023-03-08 10:55:52 -0500

    Add two test projects to database and show projects on Dashboard page

--- server/routes/project.js
+++ server/routes/project.js
@@ -5,10 +5,18 @@ const router = express.Router();
 
 router.post('/add_project', ProjectController.addProject)
 router.get('/:id', ProjectController.findOneProject)
-router.get('/dashboard', ProjectController.getAllProjects)
-// Test with no verification step
-//router.get('/dashboard/:userId', ProjectController.getAllProjects)
+router.post('/dashboard', ProjectController.getAllProjects)
 router.patch('/update/:id', ProjectController.updateProject)
 router.delete('/delete/:id', ProjectController.deleteProject)
+
+// Test with no verification step
+//router.get('/dashboard/:userId', ProjectController.getAllProjects)
+
+/* Use for POSTMAN test
+router.get('/:id', ProjectController.findOneProject)
+router.get('/dashboard', ProjectController.getAllProjects)
+router.patch('/update/:id', ProjectController.updateProject)
+router.delete('/delete/:id', ProjectController.deleteProject)
+*/
 
 module.exports = router;
