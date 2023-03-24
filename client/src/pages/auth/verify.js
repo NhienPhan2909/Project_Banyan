f9b573d063889c69dd92f48ab3eb93325608ab39
Aaron Rodrigues <ajr242@case.edu>
2023-03-23 21:04:39 -0400

    Making activation tokens less ambiguous, and streamlining verification process

--- client/src/pages/auth/verify.js
+++ client/src/pages/auth/verify.js
@@ -15,17 +15,24 @@ class Verify extends Component {
     };
 
     displayErrorMessage = () => {
-        return <Box display='flex' justifyContent='center' color='red'  alignItems='center' margin-top='10px'>Please check your login credentials</Box>
+        return <Box display='flex' justifyContent='center' color='red' alignItems='center' margin-top='10px'>Please check your login credentials</Box>
     }
-    
+
     displaySuccess = () => {
-        return <Box display='flex' justifyContent='center' color='green'  alignItems='center' margin-top='10px'>Sucess! Redirected in 5 seconds.</Box>
+        return <Box display='flex' justifyContent='center' color='green' alignItems='center' margin-top='10px'>Sucess! Redirected in 3 seconds.</Box>
     }
 
     componentDidMount() {
         this.callBackendAPI()
             .then(res => this.setState({ data: res.express }))
             .catch(err => console.log(err));
+        this.runVerification();
+    }
+
+    // Have to use a helper because of async function verify
+    async runVerification() {
+        const status = await this.verify();
+        this.setState({ status: status });
     }
 
     // fetching the GET route from the Express server which matches the GET route from server.js
@@ -34,16 +41,16 @@ class Verify extends Component {
         const body = await response.json();
 
         if (response.status !== 200) {
-            throw Error(body.message) 
+            throw Error(body.message)
         }
         return body;
     };
 
     verify = async () => {
         try {
-            var token = window.location.search.replace('?token=', '');
+            var activationToken = window.location.search.replace('?token=', '');
             const response = await axios.post('http://localhost:11000/api/verify', {
-                token
+                activationToken
             });
 
             console.log(response);
@@ -53,8 +60,8 @@ class Verify extends Component {
                 localStorage.setItem('jwtToken', response.data.token);
                 axios.defaults.headers.common['Authorization'] = `Bearer ${response.data.token}`;
 
-                var interval = setInterval(myURL, 5000);
-                function myURL () {
+                var interval = setInterval(myURL, 3000);
+                function myURL() {
                     window.location.href = '/prompt'
                 }
             }
@@ -65,48 +72,42 @@ class Verify extends Component {
     }
 
     render() {
-            return (
-                <div>
-              <div className="form-container">
-                <form className="form">
-                  <div className="Auth-form-content">
-                    <h3 className="Auth-form-title">Verification</h3>
-                    <div className="inputs">
-                    <div className="form-group mt-3">
-                    </div>
-                    <div className="form-group mt-3">
-                    </div>
-                    </div>
-                    <div className="authSubmit">
-                    <Stack>
-                        <Button style = {{maxWidth: '120', maxHeight: '40px', minWidth: '80px', minHeight: '40px'}} variant="contained"   
-                            onClick={async () => {
-                                var message = await this.verify()
-                                this.setState({ status: message });
-                            }}
-                        >Verify Email
-                        </Button>
-                    </Stack>
-                    </div>
-                    <p className="text-center mt-2">
-                      <a href="#">Resend Verification Email?</a>
-                    </p>
-                  </div>
-                </form>
-              </div>
-              {this.state.status !== 200 && this.state.status !== null && (
-                <div>
-                {this.displayErrorMessage()}
+        return (
+            <div>
+                <div className="form-container">
+                    <form className="form">
+                        <div className="Auth-form-content">
+                            <h3 className="Auth-form-title">Verification</h3>
+                            <div className="inputs">
+                                <div className="form-group mt-3">
+                                </div>
+                                <div className="form-group mt-3">
+                                </div>
+                            </div>
+                            <div className="authSubmit">
+                                <Stack>
+                                    Verifying email...
+                                </Stack>
+                            </div>
+                            <p className="text-center mt-2">
+                                <a href="#">Resend Verification Email?</a>
+                            </p>
+                        </div>
+                    </form>
                 </div>
+                {this.state.status !== 200 && this.state.status !== null && (
+                    <div>
+                        {this.displayErrorMessage()}
+                    </div>
                 )}
-              {this.state.sucess === true && (
-                <div>
-                    {this.displaySuccess()}
-                </div>
-              )}      
-              </div>
-            )
-          }
+                {this.state.sucess === true && (
+                    <div>
+                        {this.displaySuccess()}
+                    </div>
+                )}
+            </div>
+        )
+    }
 }
 
 export default Verify;
