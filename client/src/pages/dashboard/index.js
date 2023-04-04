3516c520fc1027694247c37ccc7e85888a2f8510
Aaron Rodrigues <ajr242@case.edu>
2023-04-04 17:27:30 -0400

    Spacing and List location to tree

--- client/src/pages/dashboard/index.js
+++ client/src/pages/dashboard/index.js
@@ -11,193 +11,193 @@ import SettingsIcon from '@mui/icons-material/Settings';
 
 class Dashboard extends Component {
 
-  state = {
-    displayNumProjectError: false,
-    anchorEl: null,
-    deleteMode: false,
-    projects: []
-  };
+    state = {
+        displayNumProjectError: false,
+        anchorEl: null,
+        deleteMode: false,
+        projects: []
+    };
 
-  componentDidMount() {
-    this.getProjects();
-  }
-
-  handlePopoverOpen = (event) => {
-    this.setState({ anchorEl: event.currentTarget });
-  };
-
-  handlePopoverClose = () => {
-    this.setState({ anchorEl: null });
-  };
-
-  handleDeleteProject = () => {
-    // implement logic to delete project
-    this.setState({ deleteMode: true });
-    this.setState({ displayNumProjectError: false });
-    
-  };
-
-  cancelDelete = () => {
-    // implement logic to delete project
-    this.setState({ deleteMode: false });
-    this.handlePopoverClose()
-  };
-
-  buttonMode = () => {
-    if (this.state.deleteMode) {
-      return <Button variant="contained" className="popover-button" sx={{ mb: 1 }} onClick={this.cancelDelete}>Cancel</Button>
-    }
-    else {
-      return <Button variant="contained" className="popover-button" sx={{ mb: 1 }} onClick={this.handleDeleteProject}>Delete</Button>
-    }
-  }
-
-  handleLogout = () => {
-    localStorage.setItem("jwtToken", "");
-    window.location.href = '/login';
-    this.handlePopoverClose();
-  };
-
-  getProjects = async () => {
-    const token = localStorage.getItem('jwtToken');
-
-    if (typeof token !== "string") {
-      console.log("Not string");
+    componentDidMount() {
+        this.getProjects();
     }
 
-    const response = await axios.post('http://localhost:11000/projects/dashboard', {
-      token
-    });
+    handlePopoverOpen = (event) => {
+        this.setState({ anchorEl: event.currentTarget });
+    };
 
-    this.setState({ projects: response.data.projects });
-  }
+    handlePopoverClose = () => {
+        this.setState({ anchorEl: null });
+    };
 
-  deleteProject = async (projectId) => {
-    try {
-      const token = localStorage.getItem('jwtToken');
+    handleDeleteProject = () => {
+        // implement logic to delete project
+        this.setState({ deleteMode: true });
+        this.setState({ displayNumProjectError: false });
 
-      // first, get the project from the database to see which nodes to delete
-      const initialResponse = await axios.get(`http://localhost:11000/projects/${projectId}`, {
-        headers: {
-          Authorization: `Bearer ${token}`
+    };
+
+    cancelDelete = () => {
+        // implement logic to delete project
+        this.setState({ deleteMode: false });
+        this.handlePopoverClose()
+    };
+
+    buttonMode = () => {
+        if (this.state.deleteMode) {
+            return <Button variant="contained" className="popover-button" sx={{ mb: 1 }} onClick={this.cancelDelete}>Cancel</Button>
         }
-      });
-
-      // delete the root nodes (this will recursively delete the child nodes)
-      const nodeResponse = await axios.delete(`http://localhost:11000/nodes/delete/${initialResponse.data._rootId}`);
-
-      // then, after all references are cleared, delete the project
-      const projectResponse = await axios.delete(`http://localhost:11000/projects/delete/${initialResponse.data._id}`, {
-        headers: {
-          Authorization: `Bearer ${token}`
+        else {
+            return <Button variant="contained" className="popover-button" sx={{ mb: 1 }} onClick={this.handleDeleteProject}>Delete</Button>
         }
-      });
-
-      // remove the project from the component state
-      this.setState({ projects: this.state.projects.filter((x) => x._id !== projectId) });
-
-      return projectResponse.status;
     }
-    catch (err) {
-      console.log(err);
-      return err.response.status;
+
+    handleLogout = () => {
+        localStorage.setItem("jwtToken", "");
+        window.location.href = '/login';
+        this.handlePopoverClose();
+    };
+
+    getProjects = async () => {
+        const token = localStorage.getItem('jwtToken');
+
+        if (typeof token !== "string") {
+            console.log("Not string");
+        }
+
+        const response = await axios.post('http://localhost:11000/projects/dashboard', {
+            token
+        });
+
+        this.setState({ projects: response.data.projects });
     }
-  };
 
-  displayHeader = () => {
-    return <div id='dash-header'>
-      <Box id='spacer' />
-      <img id='logo' src="BanyanText_Transparent.png" />
-      <Box id='settings'>
-        <Button onClick={this.handlePopoverOpen}>
-          <SettingsIcon sx={{ color: 'rgb(21, 20, 20)', fontSize: '50px' }} />
-        </Button>
-      </Box>
-      <Popover
-        open={Boolean(this.state.anchorEl)}
-        anchorEl={this.state.anchorEl}
-        onClose={this.handlePopoverClose}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'center',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'center',
-        }}
-      >
-        <Box display={'flex'} flexDirection={'column'} sx={{ p: 2, pb: 0, gap: 0.2 }}>
-          {this.buttonMode()}
-        </Box>
-        <Box display={'flex'} flexDirection={'column'} sx={{ p: 2, pt: 0, gap: 0.2 }}>
-          <Button className="popover-button" variant="contained" onClick={this.handleLogout}>
-            Logout
-          </Button>
-        </Box>
-      </Popover>
-    </div>
-  }
+    deleteProject = async (projectId) => {
+        try {
+            const token = localStorage.getItem('jwtToken');
 
-  displayProjects = () => {
-    return <Box id="dash-projects">
-      <div id="projects-text">Your Projects</div>
-      <Stack direction="row" divider={<Divider orientation="vertical" flexItem />} spacing={2}>
-        <Button className="project-button" id="NewProjButton" variant='contained'
-          onClick={() => {
-            if (this.state.projects.length >= 4) {
-              this.setState({ displayNumProjectError: true })
-            }
-            else {
-              window.location.href = '/prompt';
-            }
-          }
-          }>+
-        </Button>
-        {this.state.projects.length && this.state.projects.map(project => (
-          <Button className="project-button" key={project._id} variant='contained'
-            onClick={() => {
-              if (this.state.deleteMode === true) {
-                if (window.confirm("Click OK to confirm that you want to delete this project") == true) {
-                  this.deleteProject(project._id)
-                  this.setState({ deleteMode: false })
-                }               
-              }
-              else {
-                window.location.href = '/tree/' + project._id;
-              }
-            }}
-          >{project.name}</Button>
-        ))}
-      </Stack>
-    </Box>
-  }
+            // first, get the project from the database to see which nodes to delete
+            const initialResponse = await axios.get(`http://localhost:11000/projects/${projectId}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
 
-  displayNumProjectError = () => {
-    if (this.state.displayNumProjectError)
-      return <Box display='flex' justifyContent='center' color='red' alignItems='center' paddingTop='100px'>
-        You may have a maximum of 4 projects, please delete a project if you wish to continue.
-      </Box>;
-  }
+            // delete the root nodes (this will recursively delete the child nodes)
+            const nodeResponse = await axios.delete(`http://localhost:11000/nodes/delete/${initialResponse.data._rootId}`);
 
-  displayDeleteMessage = () => {
-    if (this.state.deleteMode === true)
-      return <Box display='flex' justifyContent='center' color='red' alignItems='center' paddingTop={'100px'} margin-top='10px'>
-        Please select a project to delete
-      </Box>;
-  }
+            // then, after all references are cleared, delete the project
+            const projectResponse = await axios.delete(`http://localhost:11000/projects/delete/${initialResponse.data._id}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
 
-  render() {
-    return (
-      <div id="dash-container">
-        <div id="dash-form">
-          {this.displayHeader()}
-          {this.displayProjects()}
-          {this.displayNumProjectError()}
-          {this.displayDeleteMessage()}
+            // remove the project from the component state
+            this.setState({ projects: this.state.projects.filter((x) => x._id !== projectId) });
+
+            return projectResponse.status;
+        }
+        catch (err) {
+            console.log(err);
+            return err.response.status;
+        }
+    };
+
+    displayHeader = () => {
+        return <div id='dash-header'>
+            <Box id='spacer' />
+            <img id='logo' src="BanyanText_Transparent.png" />
+            <Box id='settings'>
+                <Button onClick={this.handlePopoverOpen}>
+                    <SettingsIcon sx={{ color: 'rgb(21, 20, 20)', fontSize: '50px' }} />
+                </Button>
+            </Box>
+            <Popover
+                open={Boolean(this.state.anchorEl)}
+                anchorEl={this.state.anchorEl}
+                onClose={this.handlePopoverClose}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'center',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center',
+                }}
+            >
+                <Box display={'flex'} flexDirection={'column'} sx={{ p: 2, pb: 0, gap: 0.2 }}>
+                    {this.buttonMode()}
+                </Box>
+                <Box display={'flex'} flexDirection={'column'} sx={{ p: 2, pt: 0, gap: 0.2 }}>
+                    <Button className="popover-button" variant="contained" onClick={this.handleLogout}>
+                        Logout
+                    </Button>
+                </Box>
+            </Popover>
         </div>
-      </div>
-    );
-  }
+    }
+
+    displayProjects = () => {
+        return <Box id="dash-projects">
+            <div id="projects-text">Your Projects</div>
+            <Stack direction="row" divider={<Divider orientation="vertical" flexItem />} spacing={2}>
+                <Button className="project-button" id="NewProjButton" variant='contained'
+                    onClick={() => {
+                        if (this.state.projects.length >= 4) {
+                            this.setState({ displayNumProjectError: true })
+                        }
+                        else {
+                            window.location.href = '/prompt';
+                        }
+                    }
+                    }>+
+                </Button>
+                {this.state.projects.length && this.state.projects.map(project => (
+                    <Button className="project-button" key={project._id} variant='contained'
+                        onClick={() => {
+                            if (this.state.deleteMode === true) {
+                                if (window.confirm("Click OK to confirm that you want to delete this project") == true) {
+                                    this.deleteProject(project._id)
+                                    this.setState({ deleteMode: false })
+                                }
+                            }
+                            else {
+                                window.location.href = '/tree/' + project._id;
+                            }
+                        }}
+                    >{project.name}</Button>
+                ))}
+            </Stack>
+        </Box>
+    }
+
+    displayNumProjectError = () => {
+        if (this.state.displayNumProjectError)
+            return <Box display='flex' justifyContent='center' color='red' alignItems='center' paddingTop='100px'>
+                You may have a maximum of 4 projects, please delete a project if you wish to continue.
+            </Box>;
+    }
+
+    displayDeleteMessage = () => {
+        if (this.state.deleteMode === true)
+            return <Box display='flex' justifyContent='center' color='red' alignItems='center' paddingTop={'100px'} margin-top='10px'>
+                Please select a project to delete
+            </Box>;
+    }
+
+    render() {
+        return (
+            <div id="dash-container">
+                <div id="dash-form">
+                    {this.displayHeader()}
+                    {this.displayProjects()}
+                    {this.displayNumProjectError()}
+                    {this.displayDeleteMessage()}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Dashboard;
