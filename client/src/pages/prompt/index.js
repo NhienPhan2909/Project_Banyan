7fdbb6cc6a43a22f258a7d0ba2a5b8bd37fe48d0
Nhien Phan <nhienphanhoang@gmail.com>
2023-04-09 21:32:57 -0400

    Add spinner as loading icon

--- client/src/pages/prompt/index.js
+++ client/src/pages/prompt/index.js
@@ -8,6 +8,7 @@ import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import axios from 'axios';
 import { fontSize } from '@mui/system';
+import FadeLoader from "react-spinners/FadeLoader";
 
 class Prompt extends Component {
     state = {
@@ -85,14 +86,14 @@ class Prompt extends Component {
     render() {
         return (
             <div>
-                {this.state.loading && <div>Loading...</div>}
+                <FadeLoader className='spinner' loading={this.state.loading} size={30} sx={{color: 'rgb(0, 105, 62)'}} style={{backgroundColor: '69af77', position: 'fixed', top: '5%', left: '50%', transform: 'translateX(-50%)',}}/>
                 <div className="prompt-container">
                     <form className="promptForm">
                     <Button className='cancel' sx={{color: 'rgb(0, 105, 62)'}} style={{backgroundColor: 'white', maxWidth: '40px', maxHeight: '30px', minWidth: '40px', minHeight: '30px', fontSize:'18px'}} variant="none"
                                         onClick={() => {
                                             window.location.href = '/dashboard'
                                         }}
-                                    >x
+                                    >
                         </Button>
                         <div className='logo'>
                             <img id='logo' src="BanyanText_Transparent.png"/>
