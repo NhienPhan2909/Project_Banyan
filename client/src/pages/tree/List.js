b7f924bcd51903985ec4df0cdb9858a5f4318bcb
Alexander Pallozzi <alexanderpallozzi@gmail.com>
2023-04-11 13:59:23 -0400

    List view header

--- client/src/pages/tree/List.js
+++ client/src/pages/tree/List.js
@@ -1,9 +1,14 @@
 import * as React from "react";
+import { Box } from '@mui/material';
 import TreeView from "@mui/lab/TreeView";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import TreeItem from "@mui/lab/TreeItem";
 import Container from "@mui/material/Container";
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import Divider from '@mui/material/Divider';
+
 
 export default function List({ data }) {
     const [selectedItemId, setSelectedItemId] = React.useState(null);
@@ -39,7 +44,29 @@ export default function List({ data }) {
     };
 
     return (
-        <Container style={{ border: "solid black", width: "20%" }}>
+
+        <Container style={{ border: "solid black", width: "20%", paddingTop: '10px' }}>
+            <img id='treeLogo' src="/BanyanText_Transparent.png" />
+
+            <Stack direction="row" divider={<Divider orientation="vertical" flexItem />} spacing={1} paddingTop={'5px'} paddingBottom={'10px'}>
+                <Button sx={{backgroundColor: 'rgb(0, 105, 62)', fontSize:'12px'}} style={{ maxWidth: '90px', maxHeight: '40px', minWidth: '90px', minHeight: '40px' }} variant="contained" 
+                onClick={async () => 
+                { 
+                    window.location.href = '/dashboard' 
+                }}>
+                 Dashboard
+                </Button>
+                <Button sx={{backgroundColor: 'rgb(0, 105, 62)', fontSize:'12px'}} style={{ maxWidth: '90px', maxHeight: '40px', minWidth: '90px', minHeight: '40px' }} variant="contained" 
+                onClick={async () => 
+                {
+                    
+                }}>
+                    Save
+                </Button>
+            </Stack>
+            <Divider sx={{ color:'black', backgroundColor:'black', my: '10px' }} />
+
+
             <h2>List View</h2>
 
             <TreeView
