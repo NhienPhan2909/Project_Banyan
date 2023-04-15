4344b02926152fc6d6a41befa5223b13d0bc453d
lat71 <lat71@case.edu>
2023-04-14 20:11:08 -0400

    Added all slides. Style Changes to on boarding

--- client/src/pages/onboard/index.js
+++ client/src/pages/onboard/index.js
@@ -3,7 +3,7 @@ import { Carousel, Container, Row, Col, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTree, faSearch, faSeedling } from '@fortawesome/free-solid-svg-icons';
+import { faTree, faSeedling } from '@fortawesome/free-solid-svg-icons';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './onboard.css'
@@ -17,7 +17,7 @@ function Onboard() {
         <Container>
           <Row>
             <Col>
-              <h1 className="text-center">Welcome to</h1>
+              <h1 className="text-center text">Welcome to</h1>
               <div id='header'>
                 <img id='logo'  src="BanyanText_Transparent.png" />
               </div>
@@ -27,82 +27,82 @@ function Onboard() {
             <Row>
               <Carousel>
                 <Carousel.Item className='custom-item'>
-                  <div id="specialDiv" >
+                  <div className="ImgDiv" >
                     <img 
-                    id="specialImg"
-                    className="d-block w-100"
+                    id="slide-1-Img"
+                    className="d-block w-100 customImg"
                     src="/slides/banyan_slide1.png"
                     alt="First slide"
                     />
                   </div>
                 </Carousel.Item>
                 <Carousel.Item>
-                  <img
-                    className="d-block w-100"
-                    src="https://picsum.photos/800/400?text=Slide%202"
-                    alt="Second slide"
-                  />
+                  <div className="ImgDiv">
+                    <img
+                      className="d-block w-100 customImg"
+                      src="/slides/signup_slide.png"
+                      alt="Second slide"
+                    />
+                  </div>
                   <Carousel.Caption>
-                    <h3>Make an Account</h3>
-                    <p>
+                    <h3 className='caption-title'>Make an Account</h3>
+                    <p className='caption-text' id='slide-2-text'>
                       Click on the button to signup with your email! Once you verify your email you can start using
-                      our product. (Maybe a cool looking security thing?)
+                      our product.
                     </p>
                   </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
-                  <img
-                    className="d-block w-100"
-                    src="https://picsum.photos/800/400?text=Slide%203"
-                    alt="Third slide"
-                  />
+                  <div className='ImgDiv'>
+                    <img
+                      className="d-block w-100 customImg"
+                      src="/slides/Prompt.gif"
+                      alt="Third slide"
+                    />
+                  </div>
                   <Carousel.Caption>
-                    <h3>Enter Your Idea!</h3>
-                    <p>
+                    <h3 className='caption-title'>Enter Your Idea!</h3>
+                    <p className='caption-text' id='slide-3-text'>
                       Give us a name and a description of your vision. The more detailed you are the better!
                       Once you submit your idea, we utilize OpenAIs GPT-3.5 to generate Agile-style Epics and Stories
-                      to give you a plan to start executing. (GIF of entering into prompt page)
+                      to give you a plan to start executing.
                     </p>
                   </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
-                  <img
-                    className="d-block w-100"
-                    src="https://picsum.photos/800/400?text=Slide%204"
-                    alt="Fourth slide"
-                  />
+                  <div className='ImgDiv'>
+                    <img
+                      className="d-block w-100 customImg"
+                      src="/slides/Tree.gif"
+                      alt="Fourth slide"
+                    />
+                  </div>
                   <Carousel.Caption>
-                    <h3>Grow Your Tree</h3> 
-                    <p>
+                    <h3 className='caption-title'>Grow Your Tree</h3> 
+                    <p className='caption-text' id='slide-4-text'>
                       Expand you tree, edit your leafs, modify your stories! The power is now in your hands to evolve
-                      the project however you see fit, or call upon GPT-3.5 to break it down even further! (GIF of expanding/editing tree)
+                      the project however you see fit, or call upon GPT-3.5 to break it down even further!
                     </p>
                   </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
-                  <img
-                    className="d-block w-100"
-                    src="https://picsum.photos/800/400?text=Slide%205"
-                    alt="Fifth slide"
-                  />
+                  <div className='ImgDiv'>
+                    <img
+                      className="d-block w-100 customImg"
+                      src="/slides/dashboard_slide.png"
+                      alt="Fifth slide"
+                    />
+                  </div>
                   <Carousel.Caption>
-                    <h3>Repeat!</h3>
-                    <p>
+                    <h3 className='caption-title' id='slide-5-title'>Repeat!</h3>
+                    <p className='caption-text' id='slide-5-text'>
                       Now the world is your oyster :). Repeat the process with as many different projects as you'd like!
-                      (Dashboard image)
                     </p>
                   </Carousel.Caption>
                 </Carousel.Item>
               </Carousel>
             </Row>
             <Row>
-              {/* <div className="features">
-                <h2>Features</h2>
-                <ul>
-                  <li>Lorem ipsum dolor sit amet</li>
-                  <li>Consectetur adipiscing elit</li>
-                  <li>Sed do eiusmod tempor incididunt</li>
-                </ul> */}
                 <div id="buttons">
                   <Link id='signup' to="/signup">
                     <Button className='signupButton' variant="dark" type="submit" block>
@@ -125,7 +125,7 @@ function Onboard() {
             </Row>
           </Row>
         </Container> 
-      </div>
+      </div> 
     </div>
   ); 
 }
