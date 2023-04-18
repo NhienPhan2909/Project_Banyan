1e622c610f92c16b43050d46eeb1cdf1a6354c68
Aaron Rodrigues <ajr242@case.edu>
2023-04-18 14:44:39 -0400

    Applying edit node to state definition

--- client/src/pages/tree/EditDialog.js
+++ client/src/pages/tree/EditDialog.js
@@ -5,67 +5,101 @@ import TextField from "@mui/material/TextField";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 
 export default function EditDialog({ open, setOpen, data, setData, selected }) {
-  const handleClose = () => {
-    setOpen(false);
-  };
+    const handleClose = () => {
+        setOpen(false);
+    };
 
-  const [name, setName] = useState("");
-  const [prompt, setPrompt] = useState("");
+    const [name, setName] = useState("");
+    const [prompt, setPrompt] = useState("");
 
-  const saveNode = () => {
-    //Update the name and prompt of the selected node
-     selected.name = name;
-     selected.attributes.prompt = prompt;
-     setOpen(false);
-   };
+    const updateNode = async (root) => {
+        try {
+            // Find the node in the PMTree given an id
+            const traverse = (node, id) => {
+                if (node.id === id) {
+                    return node;
+                }
+                if (node.children && node.children.length > 0) {
+                    for (let i = 0; i < node.children.length; i++) {
+                        const result = traverse(node.children[i], id);
+                        if (result) {
+                            return result;
+                        }
+                    }
+                }
+                return null;
+            };
 
-  useEffect(() => {
-    if(!selected) return;
-    setName(selected?.name);
-    setPrompt(selected?.attributes?.prompt);
-  }, [selected, data]);
+            //Update the name and prompt of the selected node
+            
+            // Edit display
+            selected.name = name;
+            selected.attributes.prompt = prompt;
+            setOpen(false);
+            console.log(selected);
 
-  return (
-    <div>
-      <Dialog open={open} onClose={handleClose}>
-        <DialogTitle >Edit Node</DialogTitle>
-        <DialogContent>
-          {/* <DialogContentText>Set Name</DialogContentText> */}
-          <TextField
-            autoFocus
-            id="name"
-            label="Name"
-            type="text"
-            fullWidth
-            variant="standard"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </DialogContent>
-        <DialogContent>
-          {/* <DialogContentText>Set Prompt</DialogContentText> */}
-          <TextField
-            autoFocus
-            id="prompt"
-            label="Prompt"
-            type="text"
-            fullWidth
-            variant="standard"
-            multiline={true}
-            rows="3"
-            value={prompt}
-            onChange={(e) => setPrompt(e.target.value)}
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={saveNode}>Apply</Button>
-        </DialogActions>
-      </Dialog>
-    </div>
-  );
+            // Edit state
+            const node = traverse(root, selected.id);
+            node.name = name;
+            node.attributes.prompt = prompt;
+            console.log(node);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+
+    useEffect(() => {
+        if (!selected) return;
+        setName(selected?.name);
+        setPrompt(selected?.attributes?.prompt);
+    }, [selected, data]);
+
+    return (
+        <div>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle >Edit Node</DialogTitle>
+                <DialogContent>
+                    {/* <DialogContentText>Set Name</DialogContentText> */}
+                    <TextField
+                        autoFocus
+                        id="name"
+                        label="Name"
+                        type="text"
+                        fullWidth
+                        variant="standard"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </DialogContent>
+                <DialogContent>
+                    {/* <DialogContentText>Set Prompt</DialogContentText> */}
+                    <TextField
+                        autoFocus
+                        id="prompt"
+                        label="Prompt"
+                        type="text"
+                        fullWidth
+                        variant="standard"
+                        multiline={true}
+                        rows="3"
+                        value={prompt}
+                        onChange={(e) => setPrompt(e.target.value)}
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Cancel</Button>
+                    <Button
+                        onClick={async () => {
+                        updateNode(data);
+                    }}>
+                        Apply
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
 }
