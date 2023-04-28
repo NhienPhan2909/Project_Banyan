341829989373ca59748b37acb9fd2691788a8137
Kevin Mikos <kxm613@case.edu>
2023-04-28 14:45:59 -0400

    auth pages refactoring

--- client/src/pages/auth/verify.js
+++ client/src/pages/auth/verify.js
@@ -1,62 +1,40 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import './auth.css';
 
 
-class Verify extends Component {
-    state = {
-        username: '',
-        password: '',
-        status: null,
-        sucess: null
-    };
+export default function Verify() {
 
-    displayErrorMessage = () => {
+    const [status, setStatus] = useState(null);
+    const [success, toggleSuccess] = useState(false);
+
+    const displayErrorMessage = () => {
         return <Box display='flex' justifyContent='center' color='red' alignItems='center' margin-top='10px'>Please check your login credentials</Box>
     }
 
-    displaySuccess = () => {
-        return <Box display='flex' justifyContent='center' color='green' alignItems='center' margin-top='10px'>Sucess! Redirected in 3 seconds.</Box>
-    }
-
-    componentDidMount() {
-        this.callBackendAPI()
-            .then(res => this.setState({ data: res.express }))
-            .catch(err => console.log(err));
-        this.runVerification();
+    const displaySuccess = () => {
+        return <Box display='flex' justifyContent='center' color='green' alignItems='center' margin-top='10px'>Success! Redirected in 3 seconds.</Box>
     }
 
     // Have to use a helper because of async function verify
-    async runVerification() {
-        const status = await this.verify();
-        this.setState({ status: status });
+    const runVerification = async () => {
+        const status = await verify();
+        setStatus(status);
     }
+    // Calling runVerification once the component mounts
+    useEffect(runVerification);
 
-    // fetching the GET route from the Express server which matches the GET route from server.js
-    callBackendAPI = async () => {
-        const response = await fetch('/verify');
-        const body = await response.json();
-
-        if (response.status !== 200) {
-            throw Error(body.message)
-        }
-        return body;
-    };
-
-    verify = async () => {
+    const verify = async () => {
         try {
             var activationToken = window.location.search.replace('?token=', '');
             const response = await axios.post('http://localhost:11000/api/verify', {
                 activationToken
             });
 
-            console.log(response);
-
             if (response.status === 200) {
-                this.state.sucess = true;
+                toggleSuccess(true);
                 localStorage.setItem('jwtToken', response.data.token);
                 axios.defaults.headers.common['Authorization'] = `Bearer ${response.data.token}`;
 
@@ -71,43 +49,39 @@ class Verify extends Component {
         }
     }
 
-    render() {
-        return (
-            <div>
-                <div className="form-container">
-                    <form className="form">
-                        <div className="Auth-form-content">
-                            <h3 className="Auth-form-title">Verification</h3>
-                            <div className="inputs">
-                                <div className="form-group mt-3">
-                                </div>
-                                <div className="form-group mt-3">
-                                </div>
+    return (
+        <div>
+            <div className="form-container">
+                <form className="form">
+                    <div className="Auth-form-content">
+                        <h3 className="Auth-form-title">Verification</h3>
+                        <div className="inputs">
+                            <div className="form-group mt-3">
                             </div>
-                            <div className="authSubmit">
-                                <Stack>
-                                    Verifying email...
-                                </Stack>
+                            <div className="form-group mt-3">
                             </div>
-                            <p className="text-center mt-2">
-                                <a href="#">Resend Verification Email?</a>
-                            </p>
                         </div>
-                    </form>
-                </div>
-                {this.state.status !== 200 && this.state.status !== null && (
-                    <div>
-                        {this.displayErrorMessage()}
+                        <div className="authSubmit">
+                            <Stack>
+                                Verifying email...
+                            </Stack>
+                        </div>
+                        <p className="text-center mt-2">
+                            <a href="#">Resend Verification Email?</a>
+                        </p>
                     </div>
-                )}
-                {this.state.sucess === true && (
-                    <div>
-                        {this.displaySuccess()}
-                    </div>
-                )}
+                </form>
             </div>
-        )
-    }
+            {status !== 200 && status !== null && (
+                <div>
+                    {displayErrorMessage()}
+                </div>
+            )}
+            {success === true && (
+                <div>
+                    {displaySuccess()}
+                </div>
+            )}
+        </div>
+    );
 }
-
-export default Verify;
